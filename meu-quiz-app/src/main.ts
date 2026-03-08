ab6d73a89425856a18be10c5f3eae3cd2a73f4af
goncalvespedroo <contato.pedrojson@gmail.com>
2026-03-08 11:37:19 -0300

    MVP concluido

--- meu-quiz-app/src/main.ts
+++ meu-quiz-app/src/main.ts
@@ -1,3 +1,7 @@
+let indicePerguntaAtual = 0;
+let pontuacao = 0;
+let listaDePerguntas: QuizQuestions[] = []; 
+
 interface QuizQuestions {
   category: string;
   question: string;
@@ -9,7 +13,41 @@ interface ApiResponse {
   results: QuizQuestions[];
 }
 
-function setupListeners() {
+async function fetchQuestions(){
+
+  const url = 'https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean';
+  const response = await fetch(url);
+  const data: ApiResponse =  await response.json();
+
+  listaDePerguntas = data.results;
+  renderizarQuiz()
+}
+
+function renderizarQuiz() {
+  const elementoApp = document.querySelector<HTMLDivElement>('#app');
+  if (!elementoApp) return;
+
+  if (indicePerguntaAtual >= listaDePerguntas.length) {
+    elementoApp.innerHTML = `<h1>Fim de jogo! Você fez ${pontuacao} pontos em um maximo de ${listaDePerguntas.length}.</h1>`;
+    return;
+  }
+
+  const perguntaAtual = listaDePerguntas[indicePerguntaAtual];
+
+  elementoApp.innerHTML = `
+      <div class="quiz-container">
+        <p>Pontos: ${pontuacao} | Pergunta: ${indicePerguntaAtual + 1}/10</p>
+        <h2 id="question">${perguntaAtual.question}</h2>
+        <div class="options">
+          <button class="btn-choice" value='True'>Verdadeiro</button>
+          <button class="btn-choice" value='False'>Falso</button>
+        </div>
+      </div>
+      `;
+  setupListeners(perguntaAtual.correct_answer);
+}
+
+function setupListeners(correct_answer: string) {
   const buttons = document.querySelectorAll('.btn-choice');
 
   buttons.forEach(button => {
@@ -18,33 +56,19 @@ function setupListeners() {
       const escolhaDoUsuario = target.value;
 
       console.log("Você escolheu:", escolhaDoUsuario);
+
+
+      if(target.value === correct_answer) {
+        alert("Acertou!");
+        pontuacao++;
+      } else {
+        alert("Errou...")
+      }
+      indicePerguntaAtual++;
+      console.log(indicePerguntaAtual);
+      renderizarQuiz();
     })
   })
-}
-
-async function fetchQuestions(){
-
-  const url = 'https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean';
-  const response = await fetch(url);
-  const data: ApiResponse =  await response.json();
-
-  const elementoApp = document.querySelector<HTMLDivElement>('#app');
-  
-  if (elementoApp) {
-    elementoApp.innerHTML = `
-        <div class="quiz-container">
-          <h2 id="question">${data.results[0].question}</h2>
-          
-          <div class="options">
-            <button class="btn-choice" value="True">Verdadeiro</button>
-            <button class="btn-choice" value="False">Falso</button>
-          </div>
-        </div>
-      `;
-    setupListeners();
-  }
-
-  console.log("Dados recebidos:", data);
 }
 
 fetchQuestions();

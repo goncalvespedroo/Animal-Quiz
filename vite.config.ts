87d471b24e893c38148333611eaf1bc0b3fba543
goncalvespedroo <contato.pedrojson@gmail.com>
2026-03-09 18:03:56 -0300

    Passando para react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react' // Adicione esse import!
 
 export default defineConfig({
   plugins: [
+    react(), // E adicione esse plugin aqui!
     tailwindcss(),
   ],
 })
